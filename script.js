ad82866282b88440afcc7663cf7626b2ac70d73c
IvanSanczewski <isanczewski@gmail.com>
2025-05-10 14:46:55 +0300

    Implement a switch with 3 cases and 3 conditionals depending on a event listener inside a forEach

--- script.js
+++ script.js
@@ -1,11 +1,98 @@
-const toggleImage = document.getElementById('close');
-const imageDimmer = document.getElementById('image');
+const image = document.getElementById('image');
+const title = document.querySelector('.title');
 
-console.log(toggleImage);
-console.log(imageDimmer);
+const contact = document.querySelector('.contact');
+const workshops = document.querySelector('.workshops')
+const form = document.querySelector('.form')
 
-toggleImage.addEventListener('click', dimImage);
+const imageDimmerList = document.querySelectorAll('.dimmer');
 
-function dimImage() {
-    imageDimmer.classList.toggle('dimmed');
-}
+imageDimmerList.forEach( imageDimmer => {
+    imageDimmer.addEventListener('click', () => {
+            const menuItem = imageDimmer.textContent;
+            console.log(menuItem);
+
+
+            switch (menuItem) {
+                case ('contact'):
+                    if (!image.classList.contains('dimmed')) {
+                        console.log('CONTACT 1');
+                        image.classList.add('dimmed');
+                        title.classList.toggle('hidden');
+                        contact.classList.toggle('hidden');
+                        break;
+                    }
+
+                    if ((image.classList.contains('dimmed')) && (
+                        (workshops.classList.contains('hidden')) &&
+                        (form.classList.contains('hidden')))) {
+                        console.log('FORM 2');
+                        image.classList.remove('dimmed');
+                        title.classList.toggle('hidden');
+                        contact.classList.toggle('hidden');
+                        break;
+                    }
+
+                    if ((image.classList.contains('dimmed')) && (
+                        (!workshops.classList.contains('hidden')) ||
+                        (!form.classList.contains('hidden')))) {
+                        console.log('FORM 3');
+                        contact.classList.toggle('hidden');
+                    }
+                    break;
+
+                case ('workshops'):
+                    if (!image.classList.contains('dimmed')) {
+                        console.log('WORKSHOPS 1');
+                        image.classList.add('dimmed');
+                        title.classList.toggle('hidden');
+                        workshops.classList.toggle('hidden');
+                        break;
+                    }
+
+                    if ((image.classList.contains('dimmed')) && (
+                        (contact.classList.contains('hidden')) &&
+                        (form.classList.contains('hidden')))) {
+                        console.log('WORKSHOPS 2');
+                        image.classList.remove('dimmed');
+                        title.classList.toggle('hidden');
+                        workshops.classList.toggle('hidden');
+                        break;
+                    }
+
+                    if ((image.classList.contains('dimmed')) && (
+                        (!contact.classList.contains('hidden')) ||
+                        (!form.classList.contains('hidden')))) {
+                        console.log('WORKSHOPS 3');
+                        workshops.classList.toggle('hidden');
+                    }
+                    break;
+
+                case ('organise a workshop'):
+                    if (!image.classList.contains('dimmed')) {
+                        console.log('FORM 1');
+                        image.classList.add('dimmed');
+                        title.classList.toggle('hidden');
+                        form.classList.toggle('hidden');
+                        break;
+                    }
+
+                    if ((image.classList.contains('dimmed')) && (
+                        (contact.classList.contains('hidden')) &&
+                        (workshops.classList.contains('hidden')))) {
+                        console.log('FORM 2');
+                        image.classList.remove('dimmed');
+                        title.classList.toggle('hidden');
+                        form.classList.toggle('hidden');
+                        break;
+                    }
+
+                    if ((image.classList.contains('dimmed')) && (
+                        (!contact.classList.contains('hidden')) ||
+                        (!workshops.classList.contains('hidden')))) {
+                        console.log('FORM 3');
+                        form.classList.toggle('hidden');
+                    }
+            }
+        })
+})
