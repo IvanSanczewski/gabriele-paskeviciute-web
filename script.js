faf887c23f311f784ad07231b7397acebe558230
IvanSanczewski <isanczewski@gmail.com>
2025-05-15 21:57:17 +0300

    Delete static image in slides-container div so it is ppopulated only dinamically from the array of images. Change image selector to slides-container so dimmmed applies to dinamically generated images.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const image = document.getElementById('image');
+const image = document.querySelector('.slides-container');
+console.log(image);
 const title = document.querySelector('.title');
 
 const contact = document.querySelector('.contact');
@@ -16,7 +17,6 @@ imageDimmerList.forEach( imageDimmer => {
             switch (menuItem) {
                 case ('contact'):
                     if (!image.classList.contains('dimmed')) {
-                        console.log('CONTACT 1');
                         image.classList.add('dimmed');
                         title.classList.toggle('hidden');
                         title.classList.toggle('visible');
@@ -28,7 +28,6 @@ imageDimmerList.forEach( imageDimmer => {
                     if ((image.classList.contains('dimmed')) && (
                         (workshops.classList.contains('hidden')) &&
                         (form.classList.contains('hidden')))) {
-                        console.log('FORM 2');
                         image.classList.remove('dimmed');
                         title.classList.toggle('hidden');
                         title.classList.toggle('visible');
@@ -40,7 +39,6 @@ imageDimmerList.forEach( imageDimmer => {
                     if ((image.classList.contains('dimmed')) && (
                         (!workshops.classList.contains('hidden')) ||
                         (!form.classList.contains('hidden')))) {
-                        console.log('FORM 3');
                         contact.classList.toggle('hidden');
                         contact.classList.toggle('visible');
                     }
@@ -48,7 +46,6 @@ imageDimmerList.forEach( imageDimmer => {
 
                 case ('workshops'):
                     if (!image.classList.contains('dimmed')) {
-                        console.log('WORKSHOPS 1');
                         image.classList.add('dimmed');
                         title.classList.toggle('hidden');
                         title.classList.toggle('visible');
@@ -60,7 +57,6 @@ imageDimmerList.forEach( imageDimmer => {
                     if ((image.classList.contains('dimmed')) && (
                         (contact.classList.contains('hidden')) &&
                         (form.classList.contains('hidden')))) {
-                        console.log('WORKSHOPS 2');
                         image.classList.remove('dimmed');
                         title.classList.toggle('hidden');
                         title.classList.toggle('visible');
@@ -72,7 +68,6 @@ imageDimmerList.forEach( imageDimmer => {
                     if ((image.classList.contains('dimmed')) && (
                         (!contact.classList.contains('hidden')) ||
                         (!form.classList.contains('hidden')))) {
-                        console.log('WORKSHOPS 3');
                         workshops.classList.toggle('hidden');
                         workshops.classList.toggle('visible');
                     }
@@ -80,7 +75,6 @@ imageDimmerList.forEach( imageDimmer => {
 
                 case ('organise a workshop'):
                     if (!image.classList.contains('dimmed')) {
-                        console.log('FORM 1');
                         image.classList.add('dimmed');
                         title.classList.toggle('hidden');
                         title.classList.toggle('visible');
@@ -92,7 +86,6 @@ imageDimmerList.forEach( imageDimmer => {
                     if ((image.classList.contains('dimmed')) && (
                         (contact.classList.contains('hidden')) &&
                         (workshops.classList.contains('hidden')))) {
-                        console.log('FORM 2');
                         image.classList.remove('dimmed');
                         title.classList.toggle('hidden');
                         title.classList.toggle('visible');
@@ -104,7 +97,6 @@ imageDimmerList.forEach( imageDimmer => {
                     if ((image.classList.contains('dimmed')) && (
                         (!contact.classList.contains('hidden')) ||
                         (!workshops.classList.contains('hidden')))) {
-                        console.log('FORM 3');
                         form.classList.toggle('hidden');
                         form.classList.toggle('visible');
                     }
@@ -118,18 +110,74 @@ imageDimmerList.forEach( imageDimmer => {
 
 const slides = [
     '/assets/ISP_KOKEDAMA_test.jpg',
-    '/assets/Gabriele-portrait-2.JPG',
-    '/assets/Gabriele-portrait-1.JPG'
+    '/assets/ISP_KOKEDAMA_WSP_389.jpg',
+    '/assets/ISP_KOKEDAMA_WSP_394.jpg',
+    '/assets/ISP_KOKEDAMA_WSP_405.jpg'
 ];
 
 let currentSlide = 0;
 let autoSlideInterval;
 
+const slidesContainer = document.querySelector('.slides-container');
+
 slides.forEach((src, index) =>{
+    const img = document.createElement('img');
     img.src = src;
     img.alt = `Kokedama ${index + 1}`;
     img.classList.add('slide');
 
     if (index === 0) img.classList.add('active');
     slidesContainer.appendChild(img);
+});
+
+
+const displaySlide = (index) => {
+    const slides = document.querySelectorAll('.slide');
+
+    // Hides actual active slide
+    slides[currentSlide].classList.remove('active');
+
+    // Finds out next index using module to control an infinite loop
+    currentSlide = (index + slides.length) % slides.length;
+
+    // Displays new slider
+    slides[currentSlide].classList.add('active');
+};
+
+const nextSlide = () => displaySlide(currentSlide + 1);
+const prevSlide = () => displaySlide(currentSlide - 1);
+
+
+// Slide control with icon arrows
+document.querySelector('.left').addEventListener('click', () => {
+    prevSlide();
+    resetInterval();
 })
+
+document.querySelector('.right').addEventListener('click', () => {
+    nextSlide();
+    resetInterval();
+})
+
+
+// Slide control with arrows keys
+document.addEventListener('keydown', (e) =>{
+    if(e.key === 'ArrowLeft') prevSlide();
+    if(e.key === 'ArrowRight') nextSlide();
+    resetInterval();
+});
+
+
+// Automatic slides
+const startInterval = () => {
+    autoSlideInterval = setInterval(nextSlide, 5000);
+};
+
+const resetInterval = () => {
+    clearInterval(autoSlideInterval);
+    startInterval();
+};;
+
+
+startInterval();
+
